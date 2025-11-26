8a2fc73eec281dd542cae15faf64b89b46c650c9
anthonyortega22 <anthonyortega@unicauca.edu.co>
2025-11-26 10:11:38 -0500

    commit-inicial

---
+++ Microservicio-Evaluacion-Hex/src/main/java/co/unicauca/MicroservicioEvaluacionHexApplication.java
@@ -0,0 +1,13 @@
+package co.unicauca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioEvaluacionHexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioEvaluacionHexApplication.class, args);
+	}
+
+}
